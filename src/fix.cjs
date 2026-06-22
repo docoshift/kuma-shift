3bd525f405b44d559f3e2f379efa265d6646743c
docoshift <nice.d.kbm@gmail.com>
2026-06-23 01:32:58 +0900

    enable pinch zoom css

--- src/fix.cjs
+++ src/fix.cjs
@@ -1,12 +1,8 @@
 const fs = require('fs');
-let c = fs.readFileSync('src/App.jsx', 'utf8');
-
-const lines = c.split('\n');
-console.log('533:', lines[532]);
-
-// 533行目を正しい内容に置き換え
-lines[532] = '                <table style={{ borderCollapse:"collapse", tableLayout:"fixed", width: NAME_W + COL_W * adminDays }}>';
-
-c = lines.join('\n');
-fs.writeFileSync('src/App.jsx', c);
+let c = fs.readFileSync('index.html', 'utf8');
+c = c.replace(
+  '</head>',
+  '  <style>html{touch-action:pan-x pan-y pinch-zoom;}</style>\n  </head>'
+);
+fs.writeFileSync('index.html', c);
 console.log('done');
